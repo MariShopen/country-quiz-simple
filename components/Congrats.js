18a110a114d63bcbce979aa2ee6ad2a0355a862a
Marina Shopina <marinashopina95@gmail.com>
2025-08-29 15:40:51 +0100

    feat: congrats

--- components/Congrats.js
+++ components/Congrats.js
@@ -1,3 +1,21 @@
 export function Congrats({ correctAnswered }) {
-  return "";
+  return `
+          <div class="congrats-container">
+            <div class="congrats-content">
+                <img
+                    src="./img/congrats.svg"
+                    alt="Congratulations"
+                    width="349"
+                    height="107"
+                />
+                <div class="congrats-title">
+                    Congrats! You completed the quiz
+                </div>
+                <div class="congrats-score">You answer ${correctAnswered}/10 correctly</div>
+                <button class="play-again-button" onclick="handleRestart()">
+                    Play Again
+                </button>
+            </div>
+        </div>
+    `;
 }
